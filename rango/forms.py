6eed62ab0a64832a2dff3deaf94c0af33b8a82ab
Martin Nolan <2313202n@student.gla.ac.uk>
2019-01-30 15:16:42 +0000

    Final Commit :)

--- rango/forms.py
+++ rango/forms.py
@@ -21,6 +21,10 @@ class PageForm(forms.ModelForm):
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+    class Meta:
+        model = Page
+        exclude = ('category',)
+
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
--- rango/views.py
+++ rango/views.py
@@ -30,25 +30,24 @@ def about(request):
         request.session.delete_test_cookie()
     print(request.method)
     print(request.user)
-    visitor_cookie_handler(request)##
+    visitor_cookie_handler(request)
     context_dict = {'boldmessage': "Rango says here is the about page."}
     context_dict['visits'] = request.session['visits']  
     return render(request, 'rango/about.html', context=context_dict)
 
-
 def show_category(request, category_name_slug):
+	context_dict = {}
+	
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:   
+		context_dict['category'] = None
+		context_dict['pages'] = None
 
-    context_dict = {}
-
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(category=category)
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        context_dict['category'] = None
-        context_dict['pages'] = None
-    return render(request, 'rango/category.html', context_dict)
+	return render(request, 'rango/category.html', context_dict)
 
 def add_category(request):
 	form = CategoryForm()
